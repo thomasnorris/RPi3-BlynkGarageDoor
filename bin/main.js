5b591e0a824877f68ba49ac059c2cf086146c8f0
thomasnorris <tom.norris135@gmail.com>
2017-10-17 13:14:52 -0400

    added led toggling; removed finish method

--- bin/main.js
+++ bin/main.js
@@ -4,11 +4,15 @@ var libraries = require('./libraries'),
 	gpio = libraries.getGpio();
 
 var vPinList = [],
-	// v_Led = new blynk.WidgetLed(_), // reference for later on
 	v0Pin = new blynk.VirtualPin(0), // master enable pin
 	v1Pin = new blynk.VirtualPin(1),
 	v2Pin = new blynk.VirtualPin(2), 
 	v3Pin = new blynk.VirtualPin(3);
+
+var vLedList = [],
+	v4Led = new blynk.WidgetLed(4),
+	v5Led = new blynk.WidgetLed(5),
+	v6Led = new blynk.WidgetLed(6)
 
 var gpioList = [],
 	g4 = new gpio(4, 'high'),
@@ -22,20 +26,22 @@ gpioList.push(g4, g17, g27); // no input gpio pins (not implemented now)
 
 // Execute funcions
 blynk.on('connect', () => {
-	blynkTriggerGpio(v1Pin, g4);
-	blynkTriggerGpio(v2Pin, g17);
-	blynkTriggerGpio(v3Pin, g27);
+	blynkTriggerGpio(v1Pin, g4, v4Led);
+	blynkTriggerGpio(v2Pin, g17, v5Led);
+	blynkTriggerGpio(v3Pin, g27, v6Led);
 	setupEnableSwitch(v0Pin);
-	finishUp();
 });
 
-function blynkTriggerGpio(trigger, gpio) {
+function blynkTriggerGpio(trigger, gpio, vLed) {
 	trigger.on('write', (value) => {
 		if (masterEnable)
 			value.toString() == 1 ? gpio.writeSync(0) : gpio.writeSync(1);
 		else
 			trigger.write(0);
 	});
+	setInterval(() => {
+		gpio.readSync() == 1 ? vLed.turnOff() : vLed.turnOn();
+	}, 500);
 }
 
 function resetEverything() {
@@ -45,6 +51,9 @@ function resetEverything() {
 	vPinList.forEach((vPin) => {
 		vPin.write(0);
 	});
+	vLedList.forEach((vLed) => {
+		vLed.turnOff();
+	});
 }
 
 function setupEnableSwitch(enable) {
@@ -53,8 +62,4 @@ function setupEnableSwitch(enable) {
 		resetEverything();
 		value == 1 ? masterEnable = true : masterEnable = false;
 	});
-}
-
-function finishUp() {
-	
 }
