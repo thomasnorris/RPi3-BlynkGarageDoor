75e5384e66b7a3bb4593c5968a060d0c09ab34e4
Thomas Norris <tom.norris135@gmail.com>
2017-10-18 23:22:21 +0000

    removed enable switch; removed 2 relays; removed leds

--- bin/main.js
+++ bin/main.js
@@ -5,72 +5,30 @@ var libraries = require('./libraries'),
 	wol = libraries.getWol();
 
 var vPinList = [],
-	v0Pin = new blynk.VirtualPin(0), // -Master enable pin
-	v1Pin = new blynk.VirtualPin(1),
-	v2Pin = new blynk.VirtualPin(2), 
-	v3Pin = new blynk.VirtualPin(3);
-
-var vLedList = [],
-	v4Led = new blynk.WidgetLED(4),
-	v5Led = new blynk.WidgetLED(5),
-	v6Led = new blynk.WidgetLED(6)
+	v0Pin = new blynk.VirtualPin(0),
+	v1Pin = new blynk.VirtualPin(1);
 
 var gpioList = [],
-	g4 = new gpio(4, 'high'), // -Must be set to 'high' for the relay board
-	g17 = new gpio(17, 'high'),
-	g27 = new gpio(27, 'high');
+	g4 = new gpio(4, 'high'); // -Must be set to 'high' for the relay board
+
+vPinList.push(v0Pin, v1Pin); // -No enable pin
+gpioList.push(g4); // -No input gpio pins (not implemented now)
 
 var LEVIATHAN_MAC = "70:8B:CD:4E:33:6A";
 
-var masterEnable = false;
-
-vPinList.push(v1Pin, v2Pin, v3Pin); // -No enable pin
-gpioList.push(g4, g17, g27); // -No input gpio pins (not implemented now)
-
-// Execute funcions
 blynk.on('connect', () => {
-	blynkTriggerGpio(v1Pin, g4, v4Led);
-	blynkTriggerGpio(v2Pin, g17, v5Led);
-	//blynkTriggerGpio(v3Pin, g27, v6Led);
-	setupEnableSwitch(v0Pin);
-
-	setupWol(v3Pin, LEVIATHAN_MAC);
+	blynkTriggerGpio(v0Pin, g4);
+	blynkTriggerWol(v1Pin, LEVIATHAN_MAC);
 });
 
-function blynkTriggerGpio(trigger, gpio, vLed) {
+function blynkTriggerGpio(trigger, gpio) {
 	trigger.on('write', (value) => {
-		if (masterEnable)
-			value.toString() == 1 ? gpio.writeSync(0) : gpio.writeSync(1);
-		else
-			trigger.write(0);
-	});
-	setInterval(() => {
-		gpio.readSync() == 1 ? vLed.turnOff() : vLed.turnOn();
-	}, 250);
-}
-
-function resetEverything() {
-	gpioList.forEach((gpio) => {
-		gpio.writeSync(1);
-	});
-	vPinList.forEach((vPin) => {
-		vPin.write(0);
-	});
-	vLedList.forEach((vLed) => {
-		vLed.turnOff();
+		value.toString() == 1 ? gpio.writeSync(0) : gpio.writeSync(1);
 	});
 }
 
-function setupEnableSwitch(trigger) {
-	trigger.write(masterEnable ? 1 : 0);
-	trigger.on('write', (value) => {
-		resetEverything();
-		value == 1 ? masterEnable = true : masterEnable = false;
-	});
-}
-
-function setupWol(trigger, wolMac) {
+function blynkTriggerWol(trigger, wolMac) {
 	trigger.on('write', () => {
-		wol.wake(LEVIATHAN_MAC, (err, res) => {});
+		wol.wake(LEVIATHAN_MAC, () => {});
 	});
 }
