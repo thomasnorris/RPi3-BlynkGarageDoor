ccf6211967f6cb52e6957fb38cabbcbfd5791d50
Thomas Norris <tom.norris135@gmail.com>
2017-10-18 13:08:20 -0400

    Re-Introduce Blynk LEDs (#2)
    * added led toggling; removed finish method
    
    * spelling matters
    
    * shortened timeout
    
    * Enable button is either one or off by default depending on the boolean
    
    * Formatting
    
    * added led toggling; removed finish method
    
    * spelling matters
    
    * shortened timeout
    
    * Enable button is either one or off by default depending on the boolean
    
    * Formatting

--- bin/main.js
+++ bin/main.js
@@ -4,38 +4,44 @@ var libraries = require('./libraries'),
 	gpio = libraries.getGpio();
 
 var vPinList = [],
-	// v_Led = new blynk.WidgetLed(_), // reference for later on
 	v0Pin = new blynk.VirtualPin(0), // master enable pin
 	v1Pin = new blynk.VirtualPin(1),
 	v2Pin = new blynk.VirtualPin(2), 
 	v3Pin = new blynk.VirtualPin(3);
+
+var vLedList = [],
+	v4Led = new blynk.WidgetLED(4),
+	v5Led = new blynk.WidgetLED(5),
+	v6Led = new blynk.WidgetLED(6)
 
 var gpioList = [],
 	g4 = new gpio(4, 'high'),
 	g17 = new gpio(17, 'high'),
 	g27 = new gpio(27, 'high');
 
-var masterEnable = true;
+var masterEnable = false;
 
 vPinList.push(v1Pin, v2Pin, v3Pin); // no enable pin
 gpioList.push(g4, g17, g27); // no input gpio pins (not implemented now)
 
 // Execute funcions
 blynk.on('connect', () => {
-	blynkTriggerGpio(v1Pin, g4);
-	blynkTriggerGpio(v2Pin, g17);
-	blynkTriggerGpio(v3Pin, g27);
+	blynkTriggerGpio(v1Pin, g4, v4Led);
+	blynkTriggerGpio(v2Pin, g17, v5Led);
+	blynkTriggerGpio(v3Pin, g27, v6Led);
 	setupEnableSwitch(v0Pin);
-	finishUp();
 });
 
-function blynkTriggerGpio(trigger, gpio) {
+function blynkTriggerGpio(trigger, gpio, vLed) {
 	trigger.on('write', (value) => {
 		if (masterEnable)
 			value.toString() == 1 ? gpio.writeSync(0) : gpio.writeSync(1);
 		else
 			trigger.write(0);
 	});
+	setInterval(() => {
+		gpio.readSync() == 1 ? vLed.turnOff() : vLed.turnOn();
+	}, 250);
 }
 
 function resetEverything() {
@@ -45,16 +51,15 @@ function resetEverything() {
 	vPinList.forEach((vPin) => {
 		vPin.write(0);
 	});
+	vLedList.forEach((vLed) => {
+		vLed.turnOff();
+	});
 }
 
 function setupEnableSwitch(enable) {
-	enable.write(0);
+	enable.write(masterEnable ? 1 : 0);
 	enable.on('write', (value) => {
 		resetEverything();
 		value == 1 ? masterEnable = true : masterEnable = false;
 	});
-}
-
-function finishUp() {
-	
 }
