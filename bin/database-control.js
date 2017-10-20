8d6ea0ec2898344fe41d5aa149cff75bdd383973
thomasnorris <tom.norris135@gmail.com>
2017-10-20 15:35:32 -0400

    formatting changes; moved constants to control class

--- bin/database-control.js
+++ bin/database-control.js
@@ -1,5 +1,18 @@
-var dbo = require('./database-operations');
 
-var fileData = dbo.loadDatabase((res) => {
-	return res;
+var dbo = require('./database-operations');
+var _constList = {
+	0: 'Date',
+	1: 'Recharge Timer',
+	2: 'Columbia Timer',
+	3: 'Well Timer',
+	4: 'Call For Heat Counter',
+}
+
+var _data;
+
+dbo.LoadDatabase(_constList, (data) => {
+	_data = data;
+	dbo.AddToDatabase(_data, _data.Date, new Date().getSeconds(), (data) => {
+		_data = data;
+	});
 });
--- bin/database-operations.js
+++ bin/database-operations.js
@@ -4,35 +4,35 @@ var _csv = require('fast-csv'),
 
 const FILE_NAME = 'Data.json';
 const FILE_PATH = __dirname + '/data/' + FILE_NAME;
-var _constList = {
-	0: 'Date',
-	1: 'Recharge Timer',
-	2: 'Columbia Timer',
-	3: 'Well Timer',
-	4: 'Call For Heat Counter'
-}
 
 module.exports = {
-	loadDatabase: function(res, err) {
+	LoadDatabase: function(_constList, callback) {
 		_fs.exists(FILE_PATH, (exists) => {
 			if (!exists) {
 				console.log(FILE_NAME + ' does not exist, initializing...');
 				_fs.openSync(FILE_PATH, 'w');
+
 				var tempData = {};
 				Object.keys(_constList).forEach((key) => {
 					tempData[_constList[key]] = [];
 				});
+				
 				module.exports.WriteToFile(tempData);
 			}
 			console.log('Loaded ' + FILE_NAME + ' successfully.');
-			res(JSON.parse(_fs.readFileSync(FILE_PATH)));
+			callback(module.exports.ReadFromFile());
 		});
 	},
-	WriteToFile: function(tempData) {
-		_fs.writeFileSync(FILE_PATH, JSON.stringify(tempData, null, '\t'));
+	WriteToFile: function(data) {
+		_fs.writeFileSync(FILE_PATH, JSON.stringify(data, null, '\t'));
 	},
-	AddToJson: function(tempData, key, value) {
-		data[key].push(value);
+	AddToDatabase: function(data, key, value, callback) {
+		key.push(value);
 		module.exports.WriteToFile(data);
+		callback(module.exports.ReadFromFile());
+
+	},
+	ReadFromFile: function() {
+		return JSON.parse(_fs.readFileSync(FILE_PATH))
 	}
 }
