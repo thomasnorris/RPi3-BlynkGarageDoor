c497a30c611fdb6f21a523777b59178e82117e26
Thomas Norris <tom.norris135@gmail.com>
2017-10-21 20:10:52 +0000

    console.log changes, reoved boolean

--- bin/database-operations.js
+++ bin/database-operations.js
@@ -3,18 +3,18 @@ var _fs = require('fs'),
 	_csvWriter = require('csv-write-stream');
 
 const DATA_PATH = __dirname + '/data/';
-const FILE_NAME = 'Data.json';
-const FILE_PATH = DATA_PATH + FILE_NAME;
-const CSV_NAME = 'Data.csv';
-const CSV_PATH = DATA_PATH + CSV_NAME;
+const DB_FILE_NAME = 'Data.json';
+const DB_FILE_PATH = DATA_PATH + DB_FILE_NAME;
+const CSV_FILE_NAME = 'Data.csv';
+const CSV_FILE_PATH = DATA_PATH + CSV_FILE_NAME;
 
 module.exports = {
 	LoadDatabase: function(constList, callback) {
-		_fs.exists(FILE_PATH, (exists) => {
+		_fs.exists(DB_FILE_PATH, (exists) => {
 			if (!exists) {
-				console.log(FILE_NAME + ' does not exist, initializing...');
-				_fs.openSync(FILE_PATH, 'w');
-				_fs.openSync(CSV_PATH, 'w');
+				console.log(DB_FILE_NAME + ' does not exist, creating...');
+				_fs.openSync(DB_FILE_PATH, 'w');
+				_fs.openSync(CSV_FILE_PATH, 'w');
 
 				var tempData = {};
 				Object.keys(constList).forEach((key) => {
@@ -22,10 +22,10 @@ module.exports = {
 				});
 
 				CreateNewCsv(tempData);
-				module.exports.WriteToDatabase(tempData, true);
+				module.exports.WriteToDatabase(tempData);
 			}
 			callback(module.exports.ReadFromFile());
-			console.log('Loaded ' + FILE_NAME + ' successfully.');
+			console.log('Loaded ' + DB_FILE_NAME + ' successfully.');
 		});
 		function CreateNewCsv(data) {
 			var csvHeaders = [];
@@ -35,28 +35,28 @@ module.exports = {
 				csvData.push(data[key]);
 			});
 			var csvWriter = _csvWriter({ headers: csvHeaders })
-			csvWriter.pipe(_fs.createWriteStream(CSV_PATH));
+			csvWriter.pipe(_fs.createWriteStream(CSV_FILE_PATH));
 			csvWriter.write(csvData);
 			csvWriter.end();
 		}
 	},
-	WriteToDatabase: function(data, firstTime) {
-		_fs.writeFileSync(FILE_PATH, JSON.stringify(data, null, '\t'));
+	WriteToDatabase: function(data) {
+		_fs.writeFileSync(DB_FILE_PATH, JSON.stringify(data, null, '\t'));
 		var csvData = {};
 		Object.keys(data).forEach((key) => {
 			csvData[key] = data[key][data[key].length - 1];
 		});
 		var csvWriter = _csvWriter({ sendHeaders: false });
-		csvWriter.pipe(_fs.createWriteStream(CSV_PATH, { flags: 'a' }));
+		csvWriter.pipe(_fs.createWriteStream(CSV_FILE_PATH, { flags: 'a' }));
 		csvWriter.write(csvData);
 		csvWriter.end();
 	},
 	AddToDatabase: function(data, key, value, callback) {
 		key.push(value);
-		module.exports.WriteToDatabase(data, false);
+		module.exports.WriteToDatabase(data);
 		callback(module.exports.ReadFromFile());
 	},
 	ReadFromFile: function() {
-		return JSON.parse(_fs.readFileSync(FILE_PATH))
+		return JSON.parse(_fs.readFileSync(DB_FILE_PATH))
 	}
 }
