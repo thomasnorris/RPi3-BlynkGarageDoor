ef0e618959d80a975473f6920ac0d2da5dafba38
thomasnorris <tom.norris135@gmail.com>
2017-10-23 13:04:48 -0400

    method for getting the current date; returns the most recently logged data to be set to blynk variables

--- bin/database-control.js
+++ bin/database-control.js
@@ -16,16 +16,29 @@ _dbo.LoadDatabase(_mapping, (recentData) => {
 	//_schedule.scheduleJob('* * * * * *', () => {
     //
 	//});
-	var values = [];
+	var recentValues = [];
 	Object.keys(recentData).forEach((key) => {
-		values.push(recentData[key]);
+		recentValues.push(recentData[key]);
 	});
-	for (var i = 1; i < values.length; i++) {
-		if (values[i] == undefined)
+	
+	for (var i = 1; i < recentValues.length; i++) {
+		if (recentValues[i] == undefined)
 			_blynkValues[i - 1] = 0;
 		else
-			_blynkValues[i - 1] = values[i];
+			_blynkValues[i - 1] = recentValues[i];
 	}	
-	console.log(_blynkValues);
-	_dbo.AddToDatabase([new Date(), 3, 53, 64, 77])
+
+	_dbo.AddToDatabase([GetCurrentDate(), _blynkValues[0], _blynkValues[1], _blynkValues[2], _blynkValues[3]]);
 });
+
+function GetCurrentDate() {
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth() + 1;
+	if (dd < 10)
+		dd = '0' + dd;
+	if (mm < 10)
+		mm = '0' + mm;
+
+	return mm + '/' + dd + '/' + today.getFullYear();
+}
