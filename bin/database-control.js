3974a6431abb6d8554af175bad9d9e6a6b0bc88b
Thomas Norris <tom.norris135@gmail.com>
2017-10-23 10:45:00 -0400

    CSV Generation (#7)
    * creates a new csv if there is no json file
    
    * writing to json also writes to a csv
    
    * console.log changes, reoved boolean
    
    * all new items are saved to the database at the same time; removed passing of the data object
    
    * abstracted csv writing
    
    * added .data to gititnore; using global _data and _headeres for everything
    
    * removed console.log on file creation, will change later
    
    * removed if/else check of writestreamargs parameter
    
    * added empty data directory
    
    * Change CSV Mapping (#6)
    
    * changed _mapping object to include dummy properties
    
    * added node-schedule to schedule jobs

--- bin/database-control.js
+++ bin/database-control.js
@@ -1,19 +1,38 @@
 
-var dbo = require('./database-operations');
-var _constList = {
-	0: 'Date',
-	1: 'Recharge Timer',
-	2: 'Columbia Timer',
-	3: 'Well Timer',
-	4: 'Call For Heat Counter',
+var _dbo = require('./database-operations');
+var _schedule = require('node-schedule');
+
+var _date = {
+	Prop: new Date()
 }
 
-var _data;
+var _recharge = {
+	Prop: new Date().getSeconds()
+}
 
-dbo.LoadDatabase(_constList, (data) => {
-	_data = data;
-	dbo.AddToDatabase(_data, _data.Date, new Date().getSeconds(), (data) => {
-		_data = data;
-		console.log(_data);
+var _columbia = {
+	Prop: new Date().getMonth()
+}
+
+var _well = {
+	Prop: new Date().getDate()
+}
+
+var _call = {
+	Prop: new Date().getYear()
+}
+
+var _mapping = {
+	'Date': _date,
+	'Recharge Timer': _recharge,
+	'Columbia Timer': _columbia,
+	'Well Timer': _well,
+	'Call For Heat Counter': _call 
+}
+
+_dbo.LoadDatabase(_mapping, () => {
+	_schedule.scheduleJob('* * * * * *', () => {
+		_mapping.Date.Prop = new Date().getSeconds();
+		_dbo.AddToDatabase(_mapping);
 	});
 });
--- bin/database-operations.js
+++ bin/database-operations.js
@@ -1,38 +1,76 @@
 
-var _fs = require('fs');
+var _fs = require('fs'),
+	_csvWriter = require('csv-write-stream');
 
 const DATA_PATH = __dirname + '/data/';
-const FILE_NAME = 'Data.json';
-const FILE_PATH = DATA_PATH + FILE_NAME;
+const DB_FILE_NAME = 'Data.json';
+const DB_FILE_PATH = DATA_PATH + DB_FILE_NAME;
+const CSV_FILE_NAME = 'Data.csv';
+const CSV_FILE_PATH = DATA_PATH + CSV_FILE_NAME;
+
+var _data;
+var _headers;
 
 module.exports = {
-	LoadDatabase: function(constList, callback) {
-		_fs.exists(FILE_PATH, (exists) => {
+	LoadDatabase: function(mapping, callback) {
+		_fs.exists(DB_FILE_PATH, (exists) => {
 			if (!exists) {
-				console.log(FILE_NAME + ' does not exist, initializing...');
-				_fs.openSync(FILE_PATH, 'w');
+				console.log('Database does not exist, creating ' + DB_FILE_NAME + ' and ' + CSV_FILE_NAME + '.');
+				_fs.openSync(DB_FILE_PATH, 'w');
+				_fs.openSync(CSV_FILE_PATH, 'w');
 
-				var tempData = {};
-				Object.keys(constList).forEach((key) => {
-					tempData[constList[key]] = [];
+				_data = {};
+				Object.keys(mapping).forEach((key) => {
+					_data[key] = [];
 				});
 
-				module.exports.WriteToFile(tempData);
+				CreateNewCsv();
+				module.exports.WriteToFiles();
+				console.log('Created successfully.');
 			}
-			callback(module.exports.ReadFromFile());
-			console.log('Loaded ' + FILE_NAME + ' successfully.');
+			_data = module.exports.ReadDataBase();
+			_headers = Object.keys(_data);
+			console.log(DB_FILE_NAME + ' loaded successfully.')
+			callback();
 		});
-	},
-	WriteToFile: function(data) {
-		_fs.writeFileSync(FILE_PATH, JSON.stringify(data, null, '\t'));
-	},
-	AddToDatabase: function(data, key, value, callback) {
-		key.push(value);
-		module.exports.WriteToFile(data);
-		callback(module.exports.ReadFromFile());
 
+		function CreateNewCsv() {
+			var tempHeaders = [];
+			var csvData = [];
+			Object.keys(_data).forEach((key) => {
+				tempHeaders.push(key);
+				csvData.push([]);
+			});
+			module.exports.CsvWriter(csvData, CSV_FILE_PATH, { headers: tempHeaders });
+		}
 	},
-	ReadFromFile: function() {
-		return JSON.parse(_fs.readFileSync(FILE_PATH))
+
+	WriteToFiles: function() {
+		_fs.writeFileSync(DB_FILE_PATH, JSON.stringify(_data, null, '\t'));
+		var csvData = {};
+		Object.keys(_data).forEach((key) => {
+			csvData[key] = _data[key][_data[key].length - 1];
+		});
+		module.exports.CsvWriter(csvData, CSV_FILE_PATH, { sendHeaders: false }, { flags: 'a' });
+	},
+
+	AddToDatabase: function(mapping) {
+		Object.keys(mapping).forEach((key) => {
+			// -TODO: Change Prop to the corrisponding path for the blynk vpin value
+			_data[key].push(mapping[key].Prop)
+		});
+		module.exports.WriteToFiles();
+		_data = module.exports.ReadDataBase();
+	},
+
+	ReadDataBase: function() {
+		return JSON.parse(_fs.readFileSync(DB_FILE_PATH))
+	},
+	
+	CsvWriter: function(csvData, filePath, csvWriterArgs, writeStreamArgs) {
+		var writer = _csvWriter(csvWriterArgs);
+		writer.pipe(_fs.createWriteStream(filePath, writeStreamArgs));
+		writer.write(csvData);
+		writer.end();
 	}
 }
