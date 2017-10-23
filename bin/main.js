4ef58750dc6589b3f55aabce4aaa11632e4ff3d2
thomasnorris <tom.norris135@gmail.com>
2017-10-23 14:20:59 -0400

    added and renamed remaining blynk vpins; removed vpin reset

--- bin/main.js
+++ bin/main.js
@@ -4,13 +4,18 @@ var	blynkLibrary = require('blynk-library'),
 	_blynk = new blynkLibrary.Blynk(blynkAuth),
 	_gpio = require('onoff').Gpio;
 
-var _vPinList = [],
-	_v0Pin = new _blynk.VirtualPin(0), // -Columbia valve
-	_v1Pin = new _blynk.VirtualPin(1), // -Override
-	_v2Pin = new _blynk.VirtualPin(2), // -Well valve
-	_v3Pin = new _blynk.VirtualPin(3), // -Recharge level
-	_v4Pin = new _blynk.VirtualPin(4); // -Recharge counter
-_vPinList.push(_v0Pin, _v1Pin, _v2Pin, _v3Pin, _v4Pin)
+var	_manualOverride = new _blynk.VirtualPin(0), 
+	_manualColumbia = new _blynk.VirtualPin(1), 
+	_manualWell = new _blynk.VirtualPin(2), 
+	_wellRechargeLevel = new _blynk.VirtualPin(3), 
+	_wellRechargeCounter = new _blynk.VirtualPin(4),
+	_columbiaLifetimeTimer = new _blynk.VirtualPin(5),
+	_usingColumbiaLed = new _blynk.WidgetLED(6),
+	_wellLifetimeTimer = new _blynk.VirtualPin(7),
+	_usingWellLed = new _blynk.VirtualPin(8),
+	_callForHeatCounter = new _blynk.VirtualPin(9),
+	_callForHeatLed = new _blynk.WidgetLED(10),
+	_boilerCallForGasLed = new _blynk.WidgetLED(11); 
 
 var _gpioList = [],
 	_g4 = new _gpio(4, 'high');
@@ -19,19 +24,13 @@ _gpioList.push(_g4);
 const RECHARGE_TIME_MINUTES = 90;
 const RECHARGE_COUNTUP_MILI = 1000;
 
-var _rechargeCounter = 1;
-
-// -Main call
 _blynk.on('connect', () => {
-	initialize();
-	blynkTriggerGpio(_v0Pin, _g4);
-	countUp(_v2Pin, _v3Pin, _v4Pin);
+	ResetAllGpio();
+	blynkTriggerGpio(_manualOverride, _g4);
+	countUp(_manualWell, _wellRechargeLevel, _wellRechargeCounter);
 });
 
-function initialize() {
-	_vPinList.forEach((vPin) => {
-		vPin.write(0);
-	});
+function ResetAllGpio() {
 	_gpioList.forEach((gpio) => {
 		gpio.writeSync(1);
 	});
@@ -45,7 +44,7 @@ function countUp(trigger, display, counter) {
 			interval = setInterval(() => {
 				i++;
 				if (i == RECHARGE_TIME_MINUTES + 1) {
-					counter.write(_rechargeCounter++);
+					//counter.write();
 					clearInterval(interval);
 				}
 				else
