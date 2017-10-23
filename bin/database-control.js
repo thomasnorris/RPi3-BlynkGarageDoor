81093a4f03e658582e58892a647a9b3db27c8040
thomasnorris <tom.norris135@gmail.com>
2017-10-23 12:38:29 -0400

    added method for getting the most recent data logged

--- bin/database-control.js
+++ bin/database-control.js
@@ -2,8 +2,6 @@
 var _dbo = require('./database-operations');
 var _schedule = require('node-schedule');
 
-
-
 var _mapping = {
 	0: 'Date',
 	1: 'Recharge Timer',
@@ -12,10 +10,22 @@ var _mapping = {
 	4: 'Call For Heat Counter'
 }
 
+var _blynkValues = [];
+
 _dbo.LoadDatabase(_mapping, (recentData) => {
 	//_schedule.scheduleJob('* * * * * *', () => {
     //
 	//});
-	console.log(recentData);
-	_dbo.AddToDatabase([new Date(), 5, 77, 55, 6]);
+	var values = [];
+	Object.keys(recentData).forEach((key) => {
+		values.push(recentData[key]);
+	});
+	for (var i = 1; i < values.length; i++) {
+		if (values[i] == undefined)
+			_blynkValues[i - 1] = 0;
+		else
+			_blynkValues[i - 1] = values[i];
+	}	
+	console.log(_blynkValues);
+	_dbo.AddToDatabase([new Date(), 3, 53, 64, 77])
 });
