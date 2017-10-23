0c0f7ace1ddc2b3c0e5cef16970bdcad2e194f24
Thomas Norris <tom.norris135@gmail.com>
2017-10-23 10:36:57 -0400

    Change CSV Mapping (#6)
    * changed _mapping object to include dummy properties
    
    * added node-schedule to schedule jobs

--- bin/database-operations.js
+++ bin/database-operations.js
@@ -12,23 +12,25 @@ var _data;
 var _headers;
 
 module.exports = {
-	LoadDatabase: function(constList, callback) {
+	LoadDatabase: function(mapping, callback) {
 		_fs.exists(DB_FILE_PATH, (exists) => {
 			if (!exists) {
-				console.log(DB_FILE_NAME + ' does not exist, creating...');
+				console.log('Database does not exist, creating ' + DB_FILE_NAME + ' and ' + CSV_FILE_NAME + '.');
 				_fs.openSync(DB_FILE_PATH, 'w');
 				_fs.openSync(CSV_FILE_PATH, 'w');
 
 				_data = {};
-				Object.keys(constList).forEach((key) => {
-					_data[constList[key]] = [];
+				Object.keys(mapping).forEach((key) => {
+					_data[key] = [];
 				});
 
 				CreateNewCsv();
 				module.exports.WriteToFiles();
+				console.log('Created successfully.');
 			}
 			_data = module.exports.ReadDataBase();
 			_headers = Object.keys(_data);
+			console.log(DB_FILE_NAME + ' loaded successfully.')
 			callback();
 		});
 
@@ -42,6 +44,7 @@ module.exports = {
 			module.exports.CsvWriter(csvData, CSV_FILE_PATH, { headers: tempHeaders });
 		}
 	},
+
 	WriteToFiles: function() {
 		_fs.writeFileSync(DB_FILE_PATH, JSON.stringify(_data, null, '\t'));
 		var csvData = {};
@@ -50,16 +53,20 @@ module.exports = {
 		});
 		module.exports.CsvWriter(csvData, CSV_FILE_PATH, { sendHeaders: false }, { flags: 'a' });
 	},
-	AddToDatabase: function(newData) {
-		for (var i = 0; i < newData.length; i++) {
-			_data[_headers[i]].push(newData[i]);
-		}
+
+	AddToDatabase: function(mapping) {
+		Object.keys(mapping).forEach((key) => {
+			// -TODO: Change Prop to the corrisponding path for the blynk vpin value
+			_data[key].push(mapping[key].Prop)
+		});
 		module.exports.WriteToFiles();
 		_data = module.exports.ReadDataBase();
 	},
+
 	ReadDataBase: function() {
 		return JSON.parse(_fs.readFileSync(DB_FILE_PATH))
 	},
+	
 	CsvWriter: function(csvData, filePath, csvWriterArgs, writeStreamArgs) {
 		var writer = _csvWriter(csvWriterArgs);
 		writer.pipe(_fs.createWriteStream(filePath, writeStreamArgs));
