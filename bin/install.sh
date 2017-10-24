f31a08ab573d4bd43f65e5afab3fc657bf324bde
Thomas Norris <tom.norris135@gmail.com>
2017-10-24 14:13:39 -0400

    Add pm2 to install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -13,6 +13,7 @@ echo -e "${BLUE}Installing Node.js and NPM...${NC}"
 sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 echo -e "${BLUE}Installing NPM packages...${NC}"
+sudo npm install pm2 -g
 sudo npm install
 
 # --Finishing up
