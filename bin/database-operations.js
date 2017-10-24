1d81c6bbb359ec944c107bea94d66576190ea630
Thomas Norris <tom.norris135@gmail.com>
2017-10-24 19:51:29 -0400

    added am/pm time parsing logic

--- bin/database-operations.js
+++ bin/database-operations.js
@@ -88,17 +88,37 @@ module.exports = {
 
 	GetCurrentDate: function() {
 		var today = new Date();
-		var dd = today.getDate();
-		var mm = today.getMonth() + 1;
-		if (dd < 10)
-			dd = '0' + dd;
-		if (mm < 10)
-			mm = '0' + mm;
+		var day = today.getDate();
+		var month = today.getMonth() + 1;
 
-		var date = mm + '-' + dd + '-' + today.getFullYear();
+		if (day < 10)
+			day = '0' + day;
+		if (month < 10)
+			month = '0' + month;
+
+		var date = month + '-' + day + '-' + today.getFullYear();
 
 		function WithTime() {
-			return  date + '-' + today.getHours() + ':' + today.getMinutes();
+			var hour = today.getHours();
+			var min = today.getMinutes();
+			var postfix;
+
+			if (min < 10)
+				min = '0' + min;
+			if (hour > 12) {
+				hour = hour - 12;
+				postfix = 'PM';
+			}
+			else if (hour == 12) 
+				postfix = 'PM';
+			else if (hour == 0) {
+			  	hour = 12;
+			  	postfix = 'PM';
+			}
+			else
+			  postfix = 'AM'
+
+			return  date + ' - ' + hour + ':' + min + ' ' + postfix;
 		}
 
 		function WithoutTime() {
