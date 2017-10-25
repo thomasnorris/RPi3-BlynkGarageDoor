82af960005032dd14dd4b4baa2e085bb1b5a60ee
thomasnorris <tom.norris135@gmail.com>
2017-10-25 11:06:47 -0400

    added converting minutes to hours and minutes method

--- bin/date-time-operations.js
+++ bin/date-time-operations.js
@@ -42,4 +42,14 @@ module.exports = {
 			WithoutTime: WithoutTime
 		}
 	}, 
+
+	ConvertMinutesToHoursAndMinutes: function(minutes) {
+		var h = Math.floor(minutes / 60);
+		var m = minutes % 60;
+
+		h = h < 10 ? '0' + h : h;
+		m = m < 10 ? '0' + m : m;
+
+		return h + ':' + m;
+	}
 }
