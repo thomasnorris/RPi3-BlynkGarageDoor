80cd928b5cd34d2e6061e865567c264258b28abf
thomasnorris <tom.norris135@gmail.com>
2017-11-08 09:41:23 -0500

    surrounded database-tests in a self-invoking function

--- bin/database-tests.js
+++ bin/database-tests.js
@@ -1,47 +1,49 @@
 
-var _dbo = require('./database-operations');
-var	_schedule = require('node-schedule');
-var	_rl = require('readline').createInterface({ input: process.stdin, output: process.stdout });
-var	_dto = require('./date-time-operations');
+(function() {
+	var _dbo = require('./database-operations');
+	var	_schedule = require('node-schedule');
+	var	_rl = require('readline').createInterface({ input: process.stdin, output: process.stdout });
+	var	_dto = require('./date-time-operations');
 
-var _mapping = require('./mapping').GetMapping();
-var _newData;
+	var _mapping = require('./mapping').GetMapping();
+	var _newData;
 
-_dbo.LoadDatabase(_mapping, (recentData) => {
-	_newData = recentData;
-	
-	Fill(() => {
-		console.log('Files created with dummy data.\nEnter \"a\" to run a test archive.\nEnter \"r\" to refresh the database.\nPress \"enter\" to exit.');
-		_rl.on('line', (input) => {
-			if (input == 'a') {
-				_dbo.CreateArchives();
-				console.log('Archive completed, exiting...');
-			} else if (input === 'r')
-				_dbo.RefreshDatabase();
-			else
-				console.log('Exiting.');
-			_rl.close();
+	_dbo.LoadDatabase(_mapping, (recentData) => {
+		_newData = recentData;
+		
+		Fill(() => {
+			console.log('Files created with dummy data.\nEnter \"a\" to run a test archive.\nEnter \"r\" to refresh the database.\nPress \"enter\" to exit.');
+			_rl.on('line', (input) => {
+				if (input == 'a') {
+					_dbo.CreateArchives();
+					console.log('Archive completed, exiting...');
+				} else if (input === 'r')
+					_dbo.RefreshDatabase();
+				else
+					console.log('Exiting.');
+				_rl.close();
+			});
 		});
-	});
-}, true);
+	}, true);
 
-function Fill(callback) {
-	var i = 0;
-	var max = 90;
-	_newData[_mapping.WELL_TIMER] = max;
-	var interval = setInterval(() => {
-		if (i != max) {
-			++_newData[_mapping.WELL_RECHARGE_COUNTER];
-			++_newData[_mapping.COLUMBIA_TIMER];
-			--_newData[_mapping.WELL_TIMER];
-			_newData[_mapping.CFH_COUNTER] = Math.floor(Math.random() * (max - 1) + i);
-			_newData[_mapping.WELL_RECHARGE_TIMER] = --_newData[_mapping.CFH_COUNTER];
-			_dbo.AddToDatabase(_newData);
-			_dbo.WriteToCsv();
-			i++;
-		} else {
-			clearInterval(interval);
-			callback();
-		}
-	}, 10);
-}
+	function Fill(callback) {
+		var i = 0;
+		var max = 90;
+		_newData[_mapping.WELL_TIMER] = max;
+		var interval = setInterval(() => {
+			if (i != max) {
+				++_newData[_mapping.WELL_RECHARGE_COUNTER];
+				++_newData[_mapping.COLUMBIA_TIMER];
+				--_newData[_mapping.WELL_TIMER];
+				_newData[_mapping.CFH_COUNTER] = Math.floor(Math.random() * (max - 1) + i);
+				_newData[_mapping.WELL_RECHARGE_TIMER] = --_newData[_mapping.CFH_COUNTER];
+				_dbo.AddToDatabase(_newData);
+				_dbo.WriteToCsv();
+				i++;
+			} else {
+				clearInterval(interval);
+				callback();
+			}
+		}, 10);
+	}
+})();
