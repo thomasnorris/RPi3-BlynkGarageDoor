939274a857aca79b793b5c2f2537075feb64311f
thomasnorris <tom.norris135@gmail.com>
2017-11-09 11:15:43 -0500

    schedule comment again

--- bin/program.js
+++ bin/program.js
@@ -275,7 +275,8 @@
 				File operations while there's a chance of reading/writing to them can be dangerous. 
 				If there is a call for heat or the well is not charged the system is most likely reading/writing data, and
 					if this happens when one of the schedules are to run there is a chance that something will break, which is 
-					super not good. This will contrinue to try and run the schedule a minute later and will continue to do so.
+					not good. 
+				This method will contrinue to try and run the schedule every minute after the initial run.
 				It will rest to default on success.
 			*/
 			var newSchedule = originalSchedule;
