204650875069a4854ab668240c5f88fe0b1f491e
thomasnorris <tom.norris135@gmail.com>
2017-12-04 10:03:01 -0500

    added date and time to the error logging; comment changes; .gitignore changes

--- bin/blynk-setup.js
+++ bin/blynk-setup.js
@@ -1,8 +1,9 @@
 
 var _outerFunc = module.exports = {
 	Setup: function(callback) {
+		var _dto = require('./date-time-operations');
 		var _fs = require('fs');
-		var _logName = 'blynk-errors.txt'
+		var _logName = 'blynk-errors.txt';
 		var _tcpPortUsed = require('tcp-port-used');
 		var _blynkLibrary = require('blynk-library');
 		//var _blynkAuth = require('./blynk-auth').GetAuth();
@@ -22,13 +23,14 @@ var _outerFunc = module.exports = {
 						connector: new _blynkLibrary.TcpClient(
 							options = { addr: blynkServerIp, port: blynkServerPort })});
 
-					blynk.on('error', (err) => {
+					// --Catch Blynk errors and log them to a file. PM2 will take care of other issues
+					blynk.on('error', (blynkErr) => {
 						_fs.stat(_logName, (err, stats) => {
 							if (!stats || stats.size === 0)
 								_fs.closeSync(_fs.openSync(_logName, 'w'));
 
 							var stream = _fs.createWriteStream(_logName, { flags: 'a' });
-							stream.write(blynkErr);
+							stream.write(_dto.GetCurrentDate().WithTime() + ': ' + blynkErr);
 							stream.end('\n')
 						})
 					});
