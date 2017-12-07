9a467d05e699faeb3145c9e7b74a5ec6f3821989
thomasnorris <tom.norris135@gmail.com>
2017-12-07 10:03:05 -0500

    added server startup

--- bin/Blynk/blynk-setup.js
+++ bin/Blynk/blynk-setup.js
@@ -7,40 +7,51 @@ var _outerFunc = module.exports = {
 		var _blynkLibrary = require('blynk-library');
 		var _blynkAuth = requireLocal('blynk-auth').GetAuth();
 
-		var _blynkErrorLogNameWithPath = __dirname + '/blynk-errors.txt';
+		var serverDirectory = __dirname + '/Server/';
 
-		// --These must match the hardware plain tcp/ip port and the ip of the server
-		var blynkServerPort = 8442; //--8442 is the default
-		var blynkServerIp = 'localhost';
+		StartServer(serverDirectory, () => {
+			var blynkErrorLogNameWithPath = __dirname + '/blynk-errors.txt';
+			// --These must match the hardware plain tcp/ip port and the ip of the server
+			var blynkServerPort = 8442; //--8442 is the default
+			var blynkServerIp = 'localhost';
+			
+			blynk = new _blynkLibrary.Blynk(_blynkAuth, options = {
+				connector: new _blynkLibrary.TcpClient(
+					options = { addr: blynkServerIp, port: blynkServerPort })});
 
-		// --Blynk will only connect once the server is up and running
-		(function CheckForServerAndSetupBlynk() {
-			_tcpPortUsed.check(blynkServerPort, blynkServerIp).then((inUse) => {
-				if (!inUse)
-					CheckForServerAndSetupBlynk();
-				else {
-					blynk = new _blynkLibrary.Blynk(_blynkAuth, options = {
-						connector: new _blynkLibrary.TcpClient(
-							options = { addr: blynkServerIp, port: blynkServerPort })});
+			// --Catch Blynk errors and log them to a file. PM2 will take care of other issues
+			blynk.on('error', (blynkErr) => {
+				_fs.stat(blynkErrorLogNameWithPath, (err, stats) => {
+					if (!stats || stats.size === 0)
+						_fs.closeSync(_fs.openSync(blynkErrorLogNameWithPath, 'w'));
 
-					// --Catch Blynk errors and log them to a file. PM2 will take care of other issues
-					blynk.on('error', (blynkErr) => {
-						_fs.stat(_blynkErrorLogNameWithPath, (err, stats) => {
-							if (!stats || stats.size === 0)
-								_fs.closeSync(_fs.openSync(_blynkErrorLogNameWithPath, 'w'));
-
-							var stream = _fs.createWriteStream(_blynkErrorLogNameWithPath, { flags: 'a' });
-							stream.write(_dto.GetCurrentDateAndTime() + ': ' + blynkErr);
-							stream.end('\n');
-						});
-					});
-
-					// --Small delay is necessary otherwise Blynk will error right away
-					setTimeout(() => {
-						callback(blynk);
-					}, 500);
-				}
+					var stream = _fs.createWriteStream(blynkErrorLogNameWithPath, { flags: 'a' });
+					stream.write(_dto.GetCurrentDateAndTime() + ': ' + blynkErr);
+					stream.end('\n');
+				});
 			});
-		})();
+
+			// --Small delay is necessary otherwise Blynk will error right away
+			setTimeout(() => {
+				callback(blynk);
+			}, 500);
+		});
+
+		function StartServer(dir, callback) {
+			var spawn = require('child_process').spawn;
+
+			var results = [];
+			var list = _fs.readdirSync(dir);
+			list.forEach((file) => {
+				file = dir + '/' + file;
+				results.push(file);
+			});
+			var serverFile = results.filter((el) => {
+				return el.match(/.+(\.jar)/);
+			}).toString();
+
+			spawn('java', ['-jar', serverFile, '-dataFolder', dir]);
+			callback();
+		}
 	}
 }
