51baf275495e3bb88be69af8b47f91fe19a8802e
thomasnorris <tom.norris135@gmail.com>
2017-12-08 10:17:20 -0500

    consts; comments

--- bin/Blynk/blynk-setup.js
+++ bin/Blynk/blynk-setup.js
@@ -5,16 +5,16 @@ module.exports = function (callback) {
 	var _blynkLibrary = require('blynk-library');
 	var _blynkAuth = requireLocal('blynk-auth').GetAuth();
 
-	var serverDirectory = __dirname + '/Server/';
-	StartServer(serverDirectory, () => {
+	const SERVER_DIRECTORY = __dirname + '/Server/';
+	StartServer(SERVER_DIRECTORY, () => {
 		// --These must match the hardware plain tcp/ip port and the ip of the server
-		// --Change in serverDirectory/server.properties
-		var blynkServerPort = 8442;
-		var blynkServerIp = 'localhost';
+		// --Change in SERVER_DIRECTORY/server.properties
+		const BLYNK_SERVER_PORT = 8442;
+		const BLYNK_SERVER_IP = 'localhost';
 
 		var blynk = new _blynkLibrary.Blynk(_blynkAuth, options = {
 			connector: new _blynkLibrary.TcpClient(
-				options = { addr: blynkServerIp, port: blynkServerPort })});
+				options = { addr: BLYNK_SERVER_IP, port: BLYNK_SERVER_PORT })});
 
 		// --Throw any blynk errors so PM2 can restart everything
 		blynk.on('error', (blynkErr) => {
@@ -27,10 +27,10 @@ module.exports = function (callback) {
 		}, 500);
 	});
 
-	// --Start the server by dynamically finding the correct server.jar file
 	function StartServer(dir, callback) {
 		var spawn = require('child_process').spawn;
 
+		// --Find the .jar file regardless of its name, avoids hardcoding the file name
 		var results = [];
 		var list = _fs.readdirSync(dir);
 		list.forEach((file) => {
@@ -41,6 +41,7 @@ module.exports = function (callback) {
 			return el.match(/.+(\.jar)/);
 		});
 
+		// --If no or more than one .jar is found, throw this, because we only need one
 		if (serverFiles.length !== 1)
 			throw ('Make sure only one server .jar is in ' + dir);
 
