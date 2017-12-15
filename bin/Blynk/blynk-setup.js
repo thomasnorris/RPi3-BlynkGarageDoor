2538def36c9a284850c8888ac365901fafe8357f
thomasnorris <tom.norris135@gmail.com>
2017-12-15 10:21:38 -0500

    blynk-setup rearranging to reduce nesting

--- bin/Blynk/blynk-setup.js
+++ bin/Blynk/blynk-setup.js
@@ -1,46 +1,44 @@
 
 var _outerFunc = module.exports = {
 	Setup: function(callback) {
-		var _dto = requireLocal('date-time-operations');
-		var _fs = require('fs');
-		var _tcpPortUsed = require('tcp-port-used');
-		var _blynkLibrary = require('blynk-library');
-		var _blynkAuth = requireLocal('blynk-auth').GetAuth();
+		var dto = requireLocal('date-time-operations');
+		var fs = require('fs');
+		var tcpPortUsed = require('tcp-port-used');
+		var blynkLibrary = require('blynk-library');
+		var blynkAuth = requireLocal('blynk-auth').GetAuth();
 
-		var _blynkErrorLogNameWithPath = __dirname + '/blynk-errors.txt';
+		var blynkErrorLogNameWithPath = __dirname + '/blynk-errors.txt';
 
 		// --These must match the hardware plain tcp/ip port and the ip of the server
 		var blynkServerPort = 8442; //--8442 is the default
 		var blynkServerIp = 'localhost';
 
 		// --Blynk will only connect once the server is up and running
-		(function CheckForServerAndSetupBlynk() {
-			_tcpPortUsed.check(blynkServerPort, blynkServerIp).then((inUse) => {
-				if (!inUse)
-					CheckForServerAndSetupBlynk();
-				else {
-					blynk = new _blynkLibrary.Blynk(_blynkAuth, options = {
-						connector: new _blynkLibrary.TcpClient(
-							options = { addr: blynkServerIp, port: blynkServerPort })});
+		tcpPortUsed.check(blynkServerPort, blynkServerIp).then((inUse) => {
+			if (inUse) {
+				blynk = new blynkLibrary.Blynk(blynkAuth, options = {
+					connector: new blynkLibrary.TcpClient(
+						options = { addr: blynkServerIp, port: blynkServerPort })});
 
-					// --Catch Blynk errors and log them to a file. PM2 will take care of other issues
-					blynk.on('error', (blynkErr) => {
-						_fs.stat(_blynkErrorLogNameWithPath, (err, stats) => {
-							if (!stats || stats.size === 0)
-								_fs.closeSync(_fs.openSync(_blynkErrorLogNameWithPath, 'w'));
+				// --Catch Blynk errors and log them to a file. PM2 will take care of other issues
+				blynk.on('error', (blynkErr) => {
+					fs.stat(blynkErrorLogNameWithPath, (err, stats) => {
+						if (!stats || stats.size === 0)
+							fs.closeSync(fs.openSync(blynkErrorLogNameWithPath, 'w'));
 
-							var stream = _fs.createWriteStream(_blynkErrorLogNameWithPath, { flags: 'a' });
-							stream.write(_dto.GetCurrentDateAndTime() + ': ' + blynkErr);
-							stream.end('\n');
-						});
+						var stream = fs.createWriteStream(blynkErrorLogNameWithPath, { flags: 'a' });
+						stream.write(dto.GetCurrentDateAndTime() + ': ' + blynkErr);
+						stream.end('\n');
 					});
+				});
 
-					// --Small delay is necessary otherwise Blynk will error right away
-					setTimeout(() => {
-						callback(blynk);
-					}, 500);
-				}
-			});
-		})();
+				// --Small delay is necessary otherwise Blynk will error right away
+				setTimeout(() => {
+					callback(blynk);
+				}, 500);
+			}
+			else
+				_outerFunc.Setup(callback)
+		});
 	}
 }
